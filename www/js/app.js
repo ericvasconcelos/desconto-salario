95b0053aca489af6e53f36586489156aa0616633
Eric Oliveira <eric.oliveira@stormsec.com.br>
2016-06-15 17:58:44 -0300

    Redirect to register after save datas. Add button to include new salary

--- www/js/app.js
+++ www/js/app.js
@@ -208,7 +208,7 @@ $('#save').on('click', function() {
 		storage.setItem('registros', JSON.stringify({"list": [newRegistro]}));
 	}
 
-	Materialize.toast('Salário gravado!', 3000) 
+	Materialize.toast('Salário gravado!', 3000, '', callRegisters); 
 });
 
 var lista = JSON.parse(storage.getItem('registros'));
@@ -228,8 +228,10 @@ if (lista) {
 $("#list").html(listaCompleta);
 
 $('.acao-limpar').on('click', function () {
-	$('#list').html('');
-	storage.removeItem('registros');
+	$('#list').fadeOut(300, function() {
+		$(this).html('');
+		storage.removeItem('registros');
+	});
 });
 
 
